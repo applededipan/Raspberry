958e49545a7a88a03f61037db4ef482e7bc8346f
sunjundedipan <sunjundedipan@163.com>
2017-01-17 08:48:32 +0800

    move include .h files from init.c to init.h

--- src/init.c
+++ src/init.c
@@ -1,29 +1,5 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <errno.h>
 
-//=============== net ==========================
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <time.h>
-#if (defined __QNX__) | (defined __QNXNTO__)
-/* QNX specific headers */
-#include <unix.h>
-#else
-/* Linux / MacOS POSIX timer headers */
-#include <sys/time.h>
-#include <time.h>
-#include <arpa/inet.h>
-#endif
-#include <inttypes.h> // For fixed-width uint8_t type
 
-#include "global.h"
-#include "wiringSerial.h"
 #include "init.h"
 
 int fd1; //fd for serial task1
@@ -39,7 +15,12 @@ bool init(char *ip, struct sockaddr_in *a, struct sockaddr_in *b)
     } 
 
     //someelse will add following ...
-
+    creatPingTarget();
     return true;
+
+}
+
+void creatPingTarget(void)
+{
 
 }
--- src/init.h
+++ src/init.h
@@ -2,6 +2,34 @@
 #ifndef INIT_H
 #define INIT_H
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <errno.h>
 
+//=============== net ==========================
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <time.h>
+#if (defined __QNX__) | (defined __QNXNTO__)
+/* QNX specific headers */
+#include <unix.h>
+#else
+/* Linux / MacOS POSIX timer headers */
+#include <sys/time.h>
+#include <time.h>
+#include <arpa/inet.h>
+#endif
+#include <inttypes.h> // For fixed-width uint8_t type
+
+#include "global.h"
+#include "wiringSerial.h"
+
+bool init(char *ip, struct sockaddr_in *a, struct sockaddr_in *b);
+void creatPingTarget(void);
  
 #endif
